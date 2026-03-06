64ae463231de19d5258bc967b2ae653cb29276ab
bhavleensingh1430 <bhavleensingh45@gmail.com>
2026-03-06 12:14:40 +0530

    Create Client API Added

--- myBackend/src/modules/user/userController.ts
+++ myBackend/src/modules/user/userController.ts
@@ -2,6 +2,24 @@ import type { Request, Response } from "express";
 import { pool } from "../../config/db";
 import bcrypt from "bcrypt";
 
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await pool.query(`
+      SELECT users.id,
+       users.name,
+       users.email,
+       roles.name as role
+FROM users
+JOIN roles ON users.role_id = roles.id;
+    `);
+    res.json(users.rows);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
 export const createManager = async (req: any, res: any) => {
   const { name, email, password } = req.body;
 
@@ -31,20 +49,65 @@ export const createManager = async (req: any, res: any) => {
   }
 };
 
-export const getAllUsers = async (req: Request, res: Response) => {
+export const createClient = async (req: any, res: any) => {
+
+  const { name, email, password } = req.body;
+
   try {
-    const users = await pool.query(`
-      SELECT users.id,
-       users.name,
-       users.email,
-       roles.name as role
-FROM users
-JOIN roles ON users.role_id = roles.id;
-    `);
-    res.json(users.rows);
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const role = await pool.query(
+      "SELECT id FROM roles WHERE name=$1",
+      ["client"]
+    );
+
+    const role_id = role.rows[0].id;
+
+    const manager_id = req.user.id;
+
+    const client = await pool.query(
+      `INSERT INTO users (name,email,password,role_id,manager_id)
+       VALUES ($1,$2,$3,$4,$5)
+       RETURNING id,name,email`,
+      [name, email, hashedPassword, role_id, manager_id]
+    );
+
+    res.status(201).json(client.rows[0]);
+
   } catch (error) {
+
+    console.log(error);
+
     res.status(500).json({
-      message: "Server error",
+      message: "Server error"
     });
+
   }
+
+};
+
+export const getManagerClients = async (req: any, res: any) => {
+
+  try {
+
+    const manager_id = req.user.id;
+
+    const clients = await pool.query(
+      `SELECT id,name,email
+       FROM users
+       WHERE manager_id=$1`,
+      [manager_id]
+    );
+
+    res.json(clients.rows);
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: "Server error"
+    });
+
+  }
+
 };
--- myBackend/src/modules/user/userRoutes.ts
+++ myBackend/src/modules/user/userRoutes.ts
@@ -3,6 +3,7 @@ import { getAllUsers } from "./userController";
 import { authenticate } from "../../middleware/authMiddleware";
 import { authorize } from "../../middleware/roleMiddleware";
 import { createManager } from "./userController";
+import { createClient } from "./userController";
 
 const router = Router();
 
@@ -55,5 +56,43 @@ router.get("/", authenticate, authorize("admin"), getAllUsers);
  *         description: Forbidden
  */
 router.post("/create-manager", authenticate, authorize("admin"), createManager);
+
+/**
+ * @swagger
+ * /users/create-client:
+ *   post:
+ *     summary: Create client (Manager only)
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Client created
+ */
+router.post("/create-client", authenticate, authorize("manager"), createClient);
+
+/**
+ * @swagger
+ * /users/clients:
+ *   get:
+ *     summary: Get manager clients
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ */
+router.get("/clients", authenticate, authorize("manager"), getManagerClients);
 
 export default router;
