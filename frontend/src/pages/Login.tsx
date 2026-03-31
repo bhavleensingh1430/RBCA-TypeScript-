251b519bd91d5c24413b383a76233ae0127a2f11
bhavleensingh1430 <bhavleensingh45@gmail.com>
2026-03-31 11:34:16 +0530

    implement authentication flow with login functionality and API integration; add routing and necessary components

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import api from "../api/axios";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await api.post("/auth/login", {
+        email,
+        password,
+      });
+
+      const token = res.data.token;
+
+      localStorage.setItem("token", token);
+
+      navigate("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
